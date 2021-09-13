9d715b542fb7135e947c6296e77440a3e74cdefb
scanales01 <scanal01@outlook.com>
2021-09-12 21:04:24 -0500

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -1,15 +1,20 @@
+#Tokenizer Code
 import sys
 
+#Check for correct amount of arguments passed
 if (len(sys.argv) != 3):
     sys.stderr.write("Argument count is incorrect.\n")
 else:
+    #Read file names from arguments
     InFile = str(sys.argv[1])
     OutFile = str(sys.argv[2])
 
+    #Open the files
     with open(InFile, "r") as InputFile:
         print("Reading " + InFile)
         with open(OutFile, "w") as OutputFile:
             print("Writing to " + OutFile)
+            #Read line and split using blank space
             for line in InputFile:
                 for token in line.split():
                     OutputFile.write(":" + token + ":\n")
