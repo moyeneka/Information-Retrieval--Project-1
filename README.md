c3547c9390da4ea7ab5f3f42ee2c3c6e6d54968f
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-09-14 00:08:06 -0500

    PLY FIRST TEST

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Information-Retrieval--Project-1
+python3 tokenize.py  990.html out1.txt
--- tokenize.py
+++ tokenize.py
@@ -1,6 +1,26 @@
 #Tokenizer Code
-import sys
 
+import ply
+from ply import lex
+import sys
+# list of TOKENS
+tokens =[
+    'doc',
+    
+]
+#  Section of code which specifies lex substitution patterns
+DIGIT  = r'[0-9]'
+#UPPERCASE = r'[A-Za-z]'
+LETTER = r'[A-Z]'
+
+# tokens DEFINITION - Section which specifies regular expressions
+t_doc = r''+LETTER+'('+LETTER+'|'+ DIGIT+')*'
+
+# Regular expression rules for our tokens
+
+def t_error(t):
+    # print("Illegal characters: ", t.value)
+    t.lexer.skip(1)
 #Check for correct amount of arguments passed
 if (len(sys.argv) != 3):
     sys.stderr.write("Argument count is incorrect.\n")
@@ -8,7 +28,8 @@ else:
     #Read file names from arguments
     InFile = str(sys.argv[1])
     OutFile = str(sys.argv[2])
-
+    
+    lexer = lex.lex()
     #Open the files
     with open(InFile, "r") as InputFile:
         print("Reading " + InFile)
@@ -16,5 +37,9 @@ else:
             print("Writing to " + OutFile)
             #Read line and split using blank space
             for line in InputFile:
-                for token in line.split():
-                    OutputFile.write(":" + token + ":\n")
+                try:
+                    lexer.input(line)
+                    for token in lexer:
+                        OutputFile.write("Words: " + token + " :\n")
+                except EOFError:
+                    break
