42e78eda8c30ae0b510b6c393857d46ef5287cbe
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-09-17 18:38:46 -0500

    More updated format

--- identifier.py
+++ identifier.py
@@ -10,18 +10,30 @@ import sys
 
 # list of TOKENS
 tokens =[
-    'id',
-    'lowercase'
+    
+    'lowercase',
+    'html',
+    'nonhtml'
+    
 ]
-
+#Another way to do 
 #  Section of code which specifies lex substitution patterns
-DIGIT  = r'[0-9]'
-LETTER = r'[A-Za-z]'
-
+#DIGIT  = r'[0-9]'
+#LETTER = r'[A-Za-z]'
 # tokens DEFINITION - Section which specifies regular expressions
-t_id = r''+LETTER+'('+LETTER+'|'+ DIGIT+')*'
+#t_id = r''+LETTER+'('+LETTER+'|'+ DIGIT+')*'
 
-@TOKEN(t_id)
+def t_html(t):
+    r'(?:</?\w+>)|(?:<.+/?>)'
+    pass
+
+
+def t_nonhtml(t):
+    r'(?:\d+.?\d+.?\d+)|\w+'
+    return t
+
+
+@TOKEN(t_nonhtml)
 def t_lowercase(t):
     t.value = t.value.lower()
     return t
@@ -38,30 +50,25 @@ else:
     #Read file names from arguments
     InFile = str(sys.argv[1])
     OutFile = str(sys.argv[2])
-    #myFile = open(sys.argv[1])
 
+    
     lexer = lex.lex()
 
     with open(InFile, "r") as InputFile:
         print("Reading " + InFile)
         with open(OutFile, "w") as OutputFile:
             print("Writing to " + OutFile)
+            
+            #reading inputs
             for line in InputFile:
                 try:
+                    #lexer stores all the tokens into lex and performs the arguments
                     lexer.input(line)
                     for token in lexer:
                         OutputFile.write("Words: " + token.value + " :\n")
                     
                 except EOFError:
                     break
+    
     InputFile.close()
     OutputFile.close() 
-# ----------------------------------------
-#  Given the following input file:
-#     1234 X Students 5Day Day5
-#  Produces the following output:
-#     Identifier: X
-#     Identifier: Students
-#     Identifier: Day
-#     Identifier: Day5
-# ----------------------------------------
